7d506cbb4b3ef57f60731be5aba63749f7e6e90c
Reza Ghanbari <ghanbari.reza94@gmail.com>
2018-09-07 03:36:04 +0430

    Change dockerfiles

--- vessel-service/main.go
+++ vessel-service/main.go
@@ -1,2 +1,46 @@
 // vessel-service/main.go
 package main
+
+import (
+	pb "shippo/vessel-service/proto/vessel"
+	"errors"
+	"context"
+)
+
+type Repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+}
+
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+// FindAvailable - checks a specification against a map of vessels,
+// if capacity and max weight are below a vessels capacity and max weight,
+// then return that vessel.
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	for _, vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel, nil
+		}
+	}
+	return nil, errors.New("no vessel found by that spec")
+}
+
+// Our grpc server handler
+type service struct {
+	repo Repository
+}
+
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+
+	// Find the next available vessel
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return err
+	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
+}
