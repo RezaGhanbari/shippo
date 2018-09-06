9dd8cedbf5656a85f4194b0c61bea51b89600143
Reza Ghanbari <ghanbari.reza94@gmail.com>
2018-09-06 19:56:20 +0430

    Change cli.go for using micro

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -3,50 +3,49 @@ package main
 
 import (
 	pb "shippo/consignment-service/proto/consignment"
+	microclient "github.com/micro/go-micro/client"
 	"io/ioutil"
 	"encoding/json"
-	"google.golang.org/grpc"
 	"log"
 	"os"
 	"context"
+	"github.com/micro/go-micro/cmd"
 )
 
 const (
-address         = "localhost:50051"
-defaultFilename = "consignment.json"
+	defaultFilename = "consignment.json"
 )
 
 func parseFile(file string) (*pb.Consignment, error) {
-	var consignemt *pb.Consignment
+	var consignment *pb.Consignment
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
 		return nil, err
 	}
-	json.Unmarshal(data, &consignemt)
-	return consignemt, nil
+	json.Unmarshal(data, &consignment)
+	return consignment, nil
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
 
-	// Contact the server and print out its response.
+	cmd.Init()
+
+	// Create new greeter client
+	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
+
+
+	// Contact the server and print out its response
 	file := defaultFilename
-	log.Println(os.Args[0])
-	if len(os.Args) > 1{
+	if len(os.Args) > 1 {
 		file = os.Args[1]
 	}
+
 	consignment, err := parseFile(file)
 	if err != nil {
 		log.Fatalf("Could not parse file: %v", err)
 	}
 
-	r, err := client.CreateConsignment(context.Background(), consignment)
+	r, err := client.CreateConsignment(context.TODO(), consignment)
 	if err != nil {
 		log.Fatalf("Could not greet: %v", err)
 	}
@@ -59,5 +58,4 @@ func main() {
 	for _, v := range getAll.Consignments {
 		log.Println(v)
 	}
-
 }
